1d08b8f570d84be7fc8ad1c111336130074dcc60
Farhan Arief Ramadhan <farhannadia.nadia@gmail.com>
2024-06-07 14:18:18 +0700

    Removed Unnecessary Test Code

--- server.js
+++ server.js
@@ -292,53 +292,6 @@ app.post('/reviewbook',
   }
 );
 
-
-
-
-
-
-
-app.get('/getpurchase', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM "Purchase";');
-    res.json(result.rows);
-    client.release();
-  } catch (err) {
-    console.error('Error executing query', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-app.get('/customer', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT username FROM Customer');
-      res.json(result.rows);
-      client.release();
-    } catch (err) {
-      console.error('Error executing query', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-app.get('/customer::username', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query("SELECT password FROM Customer WHERE username = " + "'" + req.params[":username"] + "';" );
-      res.json(result.rows);
-      client.release();
-    } catch (err) {
-      console.error('Error executing query', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-app.get("/", (req, res) => {
-    console.log("get called")
-    res.send("wow")
-})
-
 app.listen(3000, (err) => {
     if (err) console.log("Error: server setup failed")
     else console.log("Server started on port 3000")
